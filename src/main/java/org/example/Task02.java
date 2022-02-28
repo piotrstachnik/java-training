6aa00afc7367bb1f7f3aa28c0496221255249496
Wojciech Siekiera <wojciech@siekiera.net>
2022-02-28 12:14:43 +0100

    Task2 - fix url.

--- src/main/java/org/example/Task02.java
+++ src/main/java/org/example/Task02.java
@@ -1,5 +1,5 @@
 package org.example;
-// https://adventofcode.com/2015/day/1
+// https://adventofcode.com/2015/day/2
 public class Task02 {
     static int getPaperNeeded(String dimensions) {
         return 0;
